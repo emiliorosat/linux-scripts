d032032b11c2c1d67acfb7a540757e13dc325c68
Emilio Rosa <me@emiliort.com>
2023-11-13 00:27:14 -0400

    delete gnome default app

--- setup-arch.sh
+++ setup-arch.sh
@@ -6,9 +6,6 @@ USERNAME=whoami
 
 echo -e "Hola $USERNAME, vamos a iniciar/n"
 
-echo -e "installing kitty terminal"
-yes | sudo pacman -S kitty
-
 echo "installing neofetch. /n"
 if [ -f "$ISNEOFETCH" ]; then
     echo -e "neofetch already exist"
@@ -36,6 +33,22 @@ git clone https://github.com/nvm-sh/nvm && cd nvm
 ./install.sh
 cd ..
 yes | sudo rm -r nvm
+
+echo "/n Install Gnome? Yes/No/n"
+if [[ $s1 -eq "yes" || $s1 -eq "y" ]] then
+    sudo pacman -S xorg xorg-server
+    sudo pacman -S gnome
+    sudo pacman -S gnome-tweaks
+    yes |  sudo packman -R nautilus epiphany
+
+
+echo -e "installing kitty terminal"
+yes | sudo pacman -S kitty
+
+echo -e "installing dolphin file manager"
+yes | sudo pacman -S dolphin
+
+fi
 
 # hyprland
 #yay -S gdb ninja gcc cmake meson libxcb xcb-proto xcb-util xcb-util-keysyms libxfixes libx11 libxcomposite xorg-xinput libxrender pixman wayland-protocols cairo pango seatd libxkbcommon xcb-util-wm xorg-xwayland libinput libliftoff libdisplay-info cpio
